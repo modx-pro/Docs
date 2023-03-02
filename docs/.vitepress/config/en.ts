ad70c035a55ece58e86393dc51ef26b9a3f34afd
Bochkarev Ivan <ivanx86@gmail.com>
2023-03-02 12:35:45 +0600

    Начало миграции документации на Vitepress

---
+++ docs/.vitepress/config/en.ts
@@ -0,0 +1,23 @@
+import type { DefaultTheme, LocaleSpecificConfig } from 'vitepress'
+
+export const META_URL = 'https://docs.modx.pro/'
+export const META_TITLE = 'Docs MODX.PRO'
+export const META_DESCRIPTION = 'META_DESCRIPTION'
+
+export const enConfig: LocaleSpecificConfig<DefaultTheme.Config> = {
+    description: META_DESCRIPTION,
+    head: [
+        ['meta', { property: 'og:url', content: META_URL }],
+        ['meta', { property: 'og:description', content: META_DESCRIPTION }],
+        ['meta', { property: 'twitter:url', content: META_URL }],
+        ['meta', { property: 'twitter:title', content: META_TITLE }],
+        ['meta', { property: 'twitter:description', content: META_DESCRIPTION }],
+    ],
+
+    themeConfig: {
+        editLink: {
+            pattern: 'https://github.com/modx-pro/Docs/edit/v2/docs/:path',
+            text: 'Suggest changes to this page',
+        },
+    },
+}
---
+++ docs/.vitepress/config/index.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "vitepress";
+import { getSidebar } from "vitepress-plugin-auto-sidebar";
+import { enConfig } from "./en";
+import { ruConfig } from "./ru";
+// import { sharedConfig } from "./shared";
+
+export default defineConfig({
+    // ...sharedConfig,
+    lastUpdated: true,
+    lastUpdatedText: "Update Date",
+    themeConfig: {
+        sidebar: getSidebar({
+            contentRoot: "/docs",
+            collapsible: true,
+            collapsed: true,
+        }),
+    },
+    locales: {
+        root: {
+            label: "Russian",
+            lang: "ru-RU",
+            link: "/",
+            ...ruConfig,
+        },
+        en: {
+            label: "English",
+            lang: "en-EN",
+            link: "/en/",
+            ...enConfig,
+        },
+    },
+});
