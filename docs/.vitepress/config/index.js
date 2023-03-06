201ecb5a653414369104eb35b6a41b88587a59cd
Bakhtovar Gulomov <gulomov@live.ru>
2023-03-06 13:26:24 +0700

    Добавлены подсветки modx и fenom

--- docs/.vitepress/config/index.js
+++ docs/.vitepress/config/index.js
@@ -4,10 +4,29 @@ import { enConfig } from "./en";
 import { ruConfig } from "./ru";
 // import { sharedConfig } from "./shared";
 
+import fenomGrammar from './fenom.tmLanguage.json';
+import modxGrammar from 'modx-tmlanguage/modx.tmLanguage.json';
+
+const modx = {
+    id: 'modx',
+    scopeName: 'text.html.modx',
+    grammar: modxGrammar,
+    aliases: ['modx'],
+}
+const fenom = {
+    id: 'fenom',
+    scopeName: 'text.html.fenom',
+    grammar: fenomGrammar,
+    aliases: ['fenom'],
+}
+
 export default defineConfig({
     base: "/docs/",
     // ...sharedConfig,
     lastUpdated: true,
+    markdown: {
+        languages: [modx, fenom],
+    },
     algolia: {
         apiKey: "6a767bbcca227a92559817e2382d8938",
         indexName: "modx",
