669178fb639c57aa4870ba45d6730b15a56d7fc4
Bakhtovar Gulomov <gulomov@live.ru>
2023-03-07 23:37:14 +0700

    Added custom theme

--- docs/.vitepress/config/ru.ts
+++ docs/.vitepress/config/ru.ts
@@ -1,11 +1,12 @@
-import type { DefaultTheme, LocaleSpecificConfig } from 'vitepress'
+import type { LocaleSpecificConfig } from 'vitepress'
+import type { DocsThemeConfig } from '../theme/types'
 import { ruMenu } from "./ruMenu";
 
 export const META_URL = 'https://docs.modx.pro/'
 export const META_TITLE = 'Docs MODX.PRO'
 export const META_DESCRIPTION = 'META_DESCRIPTION'
 
-export const ruConfig: LocaleSpecificConfig<DefaultTheme.Config> = {
+export const ruConfig: LocaleSpecificConfig<DocsThemeConfig> = {
     title: 'Docs MODX.pro',
     description: META_DESCRIPTION,
     head: [
@@ -14,6 +15,9 @@ export const ruConfig: LocaleSpecificConfig<DefaultTheme.Config> = {
         ['meta', { property: 'twitter:url', content: META_URL }],
         ['meta', { property: 'twitter:title', content: META_TITLE }],
         ['meta', { property: 'twitter:description', content: META_DESCRIPTION }],
+        ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
+        ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }],
+        ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap' }],
     ],
 
     themeConfig: {
@@ -23,6 +27,7 @@ export const ruConfig: LocaleSpecificConfig<DefaultTheme.Config> = {
         sidebarMenuLabel: 'Меню',
         darkModeSwitchLabel: 'Тема',
         lastUpdatedText: 'Последнее обновление',
+        ecosystemLabel: 'Другие продукты',
         docFooter: {
             prev: 'Предыдущая страница',
             next: 'Следующая страница'
---
+++ docs/.vitepress/theme/index.js
@@ -0,0 +1,9 @@
+import DefaultTheme from 'vitepress/theme'
+import Layout from './Layout.vue'
+import './theme.css'
+
+export default {
+    ...DefaultTheme,
+
+    Layout,
+}
