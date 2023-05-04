70685264f6a20078f7a8669bb87d9f3f85545d3d
Bakhtovar Gulomov <gulomov@live.ru>
2023-05-04 16:43:21 +0700

    feat: Скролл сайдбара до компонента при переходе из страницы компонентов

--- docs/.vitepress/theme/components/DocsLayout.vue
+++ docs/.vitepress/theme/components/DocsLayout.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import DefaultTheme from 'vitepress/theme'
+import { watch } from 'vue'
+import { useRouter, inBrowser, withBase } from 'vitepress'
+import { normalize } from 'vitepress/dist/client/shared'
 
 import DocsEcosystem from './DocsEcosystem.vue'
 import DocsComponentWidget from './DocsComponentWidget.vue'
@@ -8,6 +11,32 @@ import DocsPrevNext from './DocsPrevNext.vue'
 import DocsNotFound from './DocsNotFound.vue'
 
 const { Layout } = DefaultTheme
+const router = useRouter()
+
+if (inBrowser) {
+  watch(() => router.route.data.relativePath, (to, from) => {
+    if (!from) {
+      return
+    }
+
+    const fromLink: string = normalize(from)
+    if (!fromLink.endsWith('components/')) {
+      return
+    }
+
+    const toLink: string = withBase(normalize(to))
+    const sidebar = document.documentElement.querySelector('.VPSidebar')
+    if (!sidebar) {
+      return
+    }
+
+    const activeLink = sidebar.querySelector(`a[href="${toLink}"]`)
+    const offset = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--vp-nav-height'))
+    sidebar.scrollTo({
+      top: activeLink.getBoundingClientRect().top - offset
+    })
+  }, { immediate: true, flush: 'post' })
+}
 </script>
 
 <template>
