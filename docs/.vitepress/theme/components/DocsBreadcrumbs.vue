20a76ea50f950e0b41d5acbb438c4c2101fe4ffe
Bakhtovar Gulomov <gulomov@live.ru>
2023-05-19 14:15:11 +0700

    feat: Новые хлебные крошки

--- docs/.vitepress/theme/components/DocsBreadcrumbs.vue
+++ docs/.vitepress/theme/components/DocsBreadcrumbs.vue
@@ -1,45 +1,82 @@
 <script setup lang="ts">
-import { useData, withBase } from 'vitepress'
-import { useBreadcrumbs } from '../composables/breadcrumbs'
+import { Ref, ref, computed } from 'vue'
+import { useData, withBase, useRoute } from 'vitepress'
+import { useMediaQuery } from '@vueuse/core'
+import type { DocsPageData } from '../plugins/component'
+import VPLink from 'vitepress/dist/client/theme-default/components/VPLink.vue'
 
-const { page, site } = useData()
-const breadcrumbs = useBreadcrumbs(page, site)
+const isSmallScreen = useMediaQuery('(max-width: 1024px)')
+const { page }: { page: Ref<DocsPageData> } = useData()
+const route = useRoute()
+
+const breadcrumbs = computed(() => {
+  if (!page.value.breadcrumbs.length) {
+    return []
+  }
+
+  if (isSmallScreen.value) {
+    const tmp = [...page.value.breadcrumbs]
+    tmp.shift()
+    tmp.pop()
+    return tmp
+  }
+
+  return page.value.breadcrumbs
+})
 </script>
 
-<template v-if="breadcrumbs.length">
+<template>
   <ol
+    v-if="breadcrumbs.length"
     class="DocsBreadcrumbs"
     itemscope itemtype="https://schema.org/BreadcrumbList"
   >
     <li
-      v-for="(breadcrumb, idx) in breadcrumbs"
+      v-for="(item, idx) in breadcrumbs"
       :key="idx"
       itemscope itemprop="itemListElement" itemtype="https://schema.org/ListItem"
       class="item"
-      :class="{ 'last': breadcrumbs.length > 1 && idx === breadcrumbs.length - 1 }"
+      :class="{
+        active: item.link && withBase(item.link) === route.path,
+        'has-children': !isSmallScreen && item.items && item.items.some(item => item.link && withBase(item.link) !== route.path),
+      }"
     >
-      <template v-if="breadcrumb.link && idx !== breadcrumbs.length - 1">
-        <a
-          :href="withBase(breadcrumb.link)"
-          :title="breadcrumb.text"
-          itemprop="item"
-          class="link"
-        >
-          <span itemprop="name" class="name">{{ breadcrumb.text }}</span>
-        </a>
+      <template v-if="item.link && withBase(item.link) !== route.path">
+        <VPLink :href="item.link" :title="item.text" itemprop="item" class="link">
+          <span itemprop="name" class="name" v-text="item.text" />
+        </VPLink>
       </template>
       <template v-else>
-        <span itemprop="name" class="name" :title="breadcrumb.text">{{ breadcrumb.text }}</span>
-        <link
-          v-if="breadcrumb.link"
-          :href="withBase(breadcrumb.link)"
-          itemprop="item"
-        >
+        <span itemprop="name" class="name" :title="item.text" v-text="item.text" />
+        <link v-if="item.link" :href="withBase(item.link)" itemprop="item">
       </template>
-      <meta
-        itemprop="position"
-        :content="idx.toString()"
+      <ul
+        v-if="
+          !isSmallScreen
+          && item.items
+          && item.items.some(item => item.link && withBase(item.link) !== route.path)
+        "
+        class="dropdown"
       >
+        <li v-for="i in item.items" class="dropdown-item">
+          <template v-if="i.link">
+            <VPLink
+              v-if="route.path !== withBase(i.link)"
+              :href="i.link"
+              :title="i.text"
+              itemprop="item"
+              class="link"
+              v-text="i.text"
+            />
+            <span
+              v-else
+              class="active"
+              v-text="i.text"
+            />
+          </template>
+        </li>
+      </ul>
+      <meta itemprop="position" :content="idx.toString()">
     </li>
   </ol>
 </template>
@@ -48,7 +85,6 @@ const breadcrumbs = useBreadcrumbs(page, site)
 .DocsBreadcrumbs {
   display: flex;
   white-space: nowrap;
-  overflow: hidden;
   column-gap: 10px;
 
   color: var(--vp-c-text-2);
@@ -56,33 +92,84 @@ const breadcrumbs = useBreadcrumbs(page, site)
   font-size: .9rem;
 }
 
-.DocsBreadcrumbs > li + li::before {
+.item {
+  position: relative;
+}
+
+@media (max-width: 1024px) {
+  .DocsBreadcrumbs {
+    overflow-x: hidden;
+  }
+
+  .item {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    flex: 0 1000 auto;
+  }
+}
+
+.item + .item::before {
   content: '/';
-  display: inline-block;
+  font-weight: normal;
   margin-right: 8px;
 }
 
-.item {
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  flex: 0 1000 auto;
+.item.has-children::after {
+  content: '';
   display: inline-block;
+  vertical-align: middle;
+  width: 0;
+  height: 0;
+  margin-left: 5px;
+  border-left: 4px solid transparent;
+  border-right: 4px solid transparent;
+  border-top: 5px solid var(--vp-c-text-3);
 }
 
-.name {
-  flex: 0 1 auto;
+.item:hover .dropdown {
+  display: block;
+}
+
+.item:hover .name {
+  position: relative;
+  z-index: 10;
+}
+
+.item:not(:last-child):hover .name {
+  overflow: visible;
+}
+
+.dropdown {
+  z-index: 5;
+  display: none;
+  position: absolute;
+  top: -12px;
+  left: -8px;
+  right: -8px;
+  width: fit-content;
+  max-width: 300px;
+  padding: 40px 20px 20px;
+  background-color: var(--vp-c-bg-elv);
+  border-radius: var(--vp-border-radius);
+  box-shadow: 0px 4px 15px rgba(47, 63, 147, 0.08);
 }
 
 .link {
+  font-weight: 500;
+  color: var(--vp-c-text-1);
   transition: color 0.25s;
 }
+
 .link:hover {
   color: var(--vp-c-green);
 }
 
-.last {
-  font-weight: bold;
-  color: var(--vp-c-text-1);
+
+.dropdown-item,
+.name {
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
 }
 </style>
--- docs/.vitepress/theme/utils.ts
+++ docs/.vitepress/theme/utils.ts
@@ -1,68 +1,65 @@
-import type { UserConfig } from 'vitepress'
-import type { DocsPageData } from './plugins/component'
+import type { DefaultTheme, UserConfig } from 'vitepress'
+import type { ComponentData, DocsPageData } from './plugins/component'
 import { normalize } from 'vitepress/dist/client/shared'
 
 import type { Author } from '../../authors'
 import { authors } from '../../authors'
-
-export declare interface PathItem {
-  text: string
-  link?: string
-}
+import { DocsTheme } from './types'
 
 export function findPath(
   pageData: DocsPageData,
   config: UserConfig,
-): PathItem[] {
-  const tree = pageData.component
-  const searchable = ensureStartingSlash(normalize(pageData.relativePath))
+): DefaultTheme.SidebarItem[] {
+  let searchable = normalize(pageData.relativePath)
 
-  const path: PathItem[] = []
-  let root: PathItem | undefined
+  const localeLinks = Object.entries(config.locales).flatMap(([key]) => ({ key, link: key === 'root' ? '/' : `${key}/` }))
+  if (localeLinks.some(({ link }) => link === searchable)) return []
 
-  if (config.themeConfig.nav) {
-    root = config.themeConfig.nav.find(item => searchable.startsWith(item.link))
+  const locale = localeLinks.find(locale => locale.link.startsWith(searchable.replace(/(^.*?\/).*$/, '$1'))) || localeLinks[0]
+  searchable = ensureStartingSlash(searchable)
+  const localeConfig: DocsTheme.Config = config.locales[locale.key].themeConfig
+  const root = localeConfig.nav.find(item => 'link' in item && searchable.startsWith(item.link.replace(/(^\/.*?\/).*$/, '$1')))
+  const path: DefaultTheme.SidebarItem[] = []
+  if (root && ('link' in root)) {
+    path.push({ text: root.text, link: root.link })
+  }
+
+  let tree: DefaultTheme.SidebarItem | ComponentData
+
+  if (pageData.component) {
+    const { title, link, items } = pageData.component
+    tree = pageData.component
+    path.push({ text: title, link, items })
   } else {
-    const localeLinks = Object.entries(config.locales).flatMap(([key]) => ({
-      key,
-      link: key === 'root' ? '/' : `${key}/`,
-    }))
-    const locale = localeLinks.find(locale => searchable.startsWith(locale.link))
-    if (locale && Object.prototype.hasOwnProperty.call(config.locales, locale.key)) {
-      root = config.locales[locale.key].themeConfig.nav.find(item => searchable.startsWith(item.link))
-    }
+    const sidebar = Object.entries(localeConfig.sidebar).find(([link]) => searchable.startsWith(link))
+    if (sidebar) tree = sidebar[1]
   }
 
-  if (root) {
-    path.push({
-      text: root.text,
-      link: root.link,
-    })
-  }
-
-  if (!tree) {
-    return []
-  }
-
-  path.push({
-    text: tree.text || tree.title,
-    link: tree.link,
-  })
+  if (!tree) return path
 
   const keyExists = (tree) => {
-    if (!tree || (typeof tree !== 'object' && !Array.isArray(tree.items))) {
+    if (!tree || (typeof tree !== 'object' && !Array.isArray(tree.items) && !Array.isArray(tree))) {
       return false
     }
-    else if (tree.hasOwnProperty('link') && tree.link === searchable) {
+    else if (tree.hasOwnProperty('link') && ensureStartingSlash(tree.link) === searchable) {
       return true
+    }
+    else if (Array.isArray(tree)) {
+      for (let i = 0; i < tree.length; i++) {
+        const item = tree[i]
+        path.push(item)
+        const result = keyExists(item)
+        if (result) {
+          return result
+        }
+
+        path.pop()
+      }
     }
     else if (Array.isArray(tree.items)) {
       for (let i = 0; i < tree.items.length; i++) {
         const item = tree.items[i]
-        path.push({
-          text: item.text,
-          link: item.link || undefined,
-        })
+        path.push(item)
         const result = keyExists(item)
         if (result) {
           return result
