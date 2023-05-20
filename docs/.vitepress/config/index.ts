0585f9b4aeb43ed4146155ade576798e7d3a7cab
Bakhtovar Gulomov <gulomov@live.ru>
2023-05-20 16:02:55 +0700

    chore: Добавлен thesemetrics

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -50,6 +50,7 @@ export default defineConfigWithTheme<DocsTheme.Config>({
     ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
     ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }],
     ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap' }],
+    ['script', { src: 'https://unpkg.com/thesemetrics@latest', async: '' }],
   ],
 
   themeConfig: {
