2aecbae5c773e253fef6e7349d6b6abfdf4a8f6d
Bakhtovar Gulomov <gulomov@live.ru>
2023-06-17 13:44:51 +0700

    chore: Обновление фавиконки

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -52,9 +52,9 @@ export default defineConfigWithTheme<DocsTheme.Config>({
     ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }],
     ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap' }],
 
-    ['link', { rel: 'icon', href: '/favicon.ico', sizes: 'any' }],
-    ['link', { rel: 'icon', href: '/icon.svg', type: 'image/svg+xml' }],
-    ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' }],
+    ['link', { rel: 'icon', href: '/favicon.ico?v=2', sizes: 'any' }],
+    ['link', { rel: 'icon', href: '/icon.svg?v=2', type: 'image/svg+xml' }],
+    ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png?v=2' }],
     ['link', { rel: 'manifest', href: '/site.webmanifest' }],
   ],
 
