7b1964a911a7adebc2ce549d99f4512c7a201acf
Bakhtovar Gulomov <gulomov@live.ru>
2023-06-22 17:33:42 +0700

    chore(build): Обновление vitepress

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,18 +1,14 @@
-import type { EnhanceAppContext, Theme } from 'vitepress'
-// @ts-expect-error
+import type { EnhanceAppContext } from 'vitepress'
 import DefaultTheme from 'vitepress/theme-without-fonts'
 import DocsLayout from './components/DocsLayout.vue'
 import DocsComponentsList from './components/DocsComponentsList.vue'
 import './styles/global.css'
 
-const theme: Theme = {
-  ...DefaultTheme,
+export default {
+  extends: DefaultTheme,
   Layout: DocsLayout,
 
   enhanceApp(ctx: EnhanceAppContext) {
-    DefaultTheme.enhanceApp(ctx)
     ctx.app.component('DocsComponentsList', DocsComponentsList)
   },
 }
-
-export default theme
